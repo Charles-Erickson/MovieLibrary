769af3878b1a702fba0171ef5bea32e7997c209e
Charles Erickson <cse621@gmail.com>
2019-05-07 14:49:38 -0500

    Built an Api controller for movies model

---
+++ MovieLibrary/MovieLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using MovieLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieLibrary.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        Movies[] movies = new Movies[]
+      {
+        new Models.Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese"},
+        new Models.Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan"},
+        new Models.Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan"},
+        new Models.Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green"},
+        new Models.Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan"}
+      };
+
+        public IEnumerable<Movies> GetAllProducts()
+        {
+            return movies;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = movies.FirstOrDefault((p) => p.MovieId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
+}
--- MovieLibrary/MovieLibrary/Models/Movies.cs
+++ MovieLibrary/MovieLibrary/Models/Movies.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,13 @@ namespace MovieLibrary.Models
 {
     public class Movies
     {
+        [Key]
+        public int MovieId { get; set; }
         [DisplayName("Film Title")]
         public string Title { get; set; }
         [DisplayName("Film Genre")]
         public string Genre { get; set; }
         [DisplayName("Film Director")]
-        public string Director { get; set; }
+        public string DirectorName { get; set; }
     }
 }
