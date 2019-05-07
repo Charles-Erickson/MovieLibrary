3aba29093fe305866f214d8ea8f04cc3c7a3c2a2
Charles Erickson <cse621@gmail.com>
2019-05-07 13:11:59 -0500

    added a model for movies

---
+++ MovieLibrary/MovieLibrary/Models/Movies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MovieLibrary.Models
+{
+    public class Movies
+    {
+        [DisplayName("Film Title")]
+        public string Title { get; set; }
+        [DisplayName("Film Genre")]
+        public string Genre { get; set; }
+        [DisplayName("Film Director")]
+        public string Director { get; set; }
+    }
+}
