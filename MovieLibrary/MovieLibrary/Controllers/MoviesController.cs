3d9ae6cfc9750bf268e5288593f3680fb2962a27
Charles Erickson <cse621@gmail.com>
2019-05-08 15:32:49 -0500

    Created api controller for movie library

--- MovieLibrary/MovieLibrary/Controllers/MoviesController.cs
+++ MovieLibrary/MovieLibrary/Controllers/MoviesController.cs
@@ -1,43 +1,104 @@
 ﻿using MovieLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using System.Web.Http.Description;
+using System.Web.Http.ModelBinding;
 
 namespace MovieLibrary.Controllers
 {
     public class MoviesController : ApiController
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        public ApplicationDbContext db = new ApplicationDbContext();
+        //public List<T> movies = new List<T>;
 
 
-
-        Movies[] movies = new Movies[]
+      public MoviesController()
       {
-        new Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese"},
-        new Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan"},
-        new Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan"},
-        new Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green"},
-        new Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan"}
-      };
+            new Movies { Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" };
+            new Movies { Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" };
+            new Movies { Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" };
+            new Movies { Title = "Pineapple Express", Genre = "Comedy", DirectorName = "David Gordon Green" };
+            new Movies { Title = "Die Hard", Genre = "Action", DirectorName = "John McTiernan" };
+      }
 
-        public IEnumerable<Movies> GetAllProducts()
+        public IEnumerable<Movies> GetAllMovies()
         {
-            return db.movies.toList;
+            return db.Movies.ToList();
         }
 
-        public IHttpActionResult GetProduct(int id)
+        public IHttpActionResult GetMovie(int id)
         {
-            var product = movies.FirstOrDefault(p => p.MovieId == id);
+            var product = db.Movies.FirstOrDefault(p => p.MovieId == id);
             if (product == null)
             {
                 return NotFound();
             }
             return Ok(product);
+        }      
+    
+    // PUT: api/Movies/5
+    [ResponseType(typeof(void))]
+    public IHttpActionResult PutMovie(int id, Movies movie)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
         }
+
+        if (id != movie.MovieId)
+        {
+            return BadRequest();
+        }
+        db.Entry(movie).State = EntityState.Modified;
+        db.SaveChanges();
+        return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    public IHttpActionResult PostMovie(Movies movies)
+    {
+      if (!ModelState.IsValid)
+      return BadRequest("Invalid data.");
+
+        using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Movies.Add(new Movies()
+                {
+                    MovieId = movies.MovieId,
+                    Title = movies.Title,
+                    Genre = movies.Genre,
+                    DirectorName=movies.DirectorName,
+                });
+
+                ctx.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+
+
+        // DELETE: api/Movies/5
+        [ResponseType(typeof(Movies))]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            Movies movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+
+            return Ok(movie);
+        }
+
     }
 }
 
--- MovieLibrary/MovieLibrary/Models/MoviesContext.cs
+++ MovieLibrary/MovieLibrary/Models/MoviesContext.cs
@@ -17,3 +17,5 @@
 //        public DbSet<Movies> Movies{ get; set; }
 //    }
 //}
+
+
